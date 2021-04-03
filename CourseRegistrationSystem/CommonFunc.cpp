92e886055e7f65e2d716497fd40eb5e498011d11
nhatruong1009 <nhattruong1009@gmail.com>
2021-04-03 21:31:48 +0700

    last

--- CourseRegistrationSystem/CommonFunc.cpp
+++ CourseRegistrationSystem/CommonFunc.cpp
@@ -1,197 +1,166 @@
-#include"Data.h"
 #include"CommonFunc.h"
-#include<string>
-std::ostream& operator<<(std::ostream& os, const Date& dt)
+#include<ctime>
+#include<locale>
+void _LText()
 {
-	os << dt.dd << '/' << dt.mm << '/' << dt.yy;
-	return os;
+	_setmode(_fileno(stdin), _O_U8TEXT);
+	_setmode(_fileno(stdout), _O_U8TEXT);
 }
-std::wostream& operator<<(std::wostream& os, const Date& dt)
+void _SText() {
+	_setmode(_fileno(stdin), _O_TEXT);
+	_setmode(_fileno(stdout), _O_TEXT);
+}
+void LStrToStr(char desination[], int size, std::wstring source) {
+	for (int i = 0; i < size; i++) {
+		desination[i] = source[i];
+	}
+}
+void LStrToStr(char desination[], int size, wchar_t* source) {
+	for (int i = 0; i < size; i++) {
+		desination[i] = source[i];
+	}
+}
+void StrToLStr(wchar_t desination[], int size, std::string source) {
+	for (int i = 0; i < size; i++) {
+		desination[i] = source[i];
+	}
+}
+void StrToLStr(wchar_t desination[], int size, char* source) {
+	for (int i = 0; i < size; i++) {
+		desination[i] = source[i];
+	}
+}
+__int64 StringToInt(wchar_t* ch) {
+	unsigned __int64 a = 0;
+	while (*ch != L'\0')
+	{
+		a = a * 10 + *ch - L'0';
+		ch++;
+	}
+	return a;
+}
+__int64 StringToInt(char* ch) {
+	unsigned __int64 a = 0;
+	while (*ch != L'\0')
+	{
+		a = a * 10 + *ch - '0';
+		ch++;
+	}
+	return a;
+}
+__int64 StringToInt(std::wstring str) {
+	unsigned __int64 a = 0;
+	for (int i = 0; i < str.length(); i++) {
+		a = a * 10 + str[i] - L'0';
+	}
+	return a;
+}
+Date StringToDate(wchar_t* ch) {
+	Date a;
+	int beg = 0;
+	int end = 0;
+	while (ch[end] != L'\0' && ch[end] != L'/')
+	{
+		end += 1;
+	}
+	if (ch[end] != L'\0') {
+		for (; beg < end; beg++) {
+			a.dd = a.dd * 10 + ch[beg] - L'0';
+		}
+		beg = end + 1;
+		end += 1;
+		while (ch[end] != L'\0' && ch[end] != L'/')
+		{
+			end += 1;
+		}
+		if (ch[end] != L'\0') {
+			for (; beg < end; beg++) {
+				a.mm = a.mm * 10 + ch[beg] - L'0';
+			}
+			beg = end + 1;
+			end += 1;
+			while (ch[end] != L'\0')
+			{
+				end += 1;
+			}
+			for (; beg < end; beg++) {
+				a.yy = a.yy * 10 + ch[beg] - L'0';
+			}
+			return a;
+		}
+	}
+	Date undentity;
+	return undentity;
+}
+Date StringToDate(char* ch) {
+	Date a;
+	int beg = 0;
+	int end = 0;
+	while (ch[end] != '\0' && ch[end] != '/')
+	{
+		end += 1;
+	}
+	if (ch[end] != '\0') {
+		for (; beg < end; beg++) {
+			a.dd = a.dd * 10 + ch[beg] - '0';
+		}
+		beg = end + 1;
+		end += 1;
+		while (ch[end] != '\0' && ch[end] != '/')
+		{
+			end += 1;
+		}
+		if (ch[end] != '\0') {
+			for (; beg < end; beg++) {
+				a.mm = a.mm * 10 + ch[beg] - '0';
+			}
+			beg = end + 1;
+			end += 1;
+			while (ch[end] != '\0')
+			{
+				end += 1;
+			}
+			for (; beg < end; beg++) {
+				a.yy = a.yy * 10 + ch[beg] - L'0';
+			}
+			return a;
+		}
+	}
+	Date undentity;
+	return undentity;
+}
+tm GetTime()
 {
-	os << dt.dd << '/' << dt.mm << '/' << dt.yy;
-	return os;
+	__time32_t now = time(0);
+	tm t;
+	_localtime32_s(&t, &now);
+	return t;
 }
-std::wostream& operator<<(std::wostream& os, const tm& t) {
-	os << t.tm_hour << ":" << t.tm_min << ":" << t.tm_sec << " " << t.tm_mday << "/" << t.tm_mon << "/" << t.tm_year;
-	return os;
+bool operator>(tm& t1, tm& t2)
+{
+	time_t a, b;
+	a = mktime(&t1);
+	b = mktime(&t2);
+	if (a > b) return 1;
+	else return 0;
 }
-
-Student StringToStudent(std::wstring str) {
-	//No, Student ID, First name, Last name, Gender, Date of Birth, Social ID
-	Student stu;
-	wchar_t* temp = nullptr;
-	int beg = str.find(L',', 0) + 1;
-	int end = str.find(L',', beg);
-
-	temp = new wchar_t[end - beg + 1];
-	temp[end - beg] = L'\0';
-	str.copy(temp, end - beg, beg);
-	_LText();
-	stu.ID = StringToInt(temp);
-	stu.account.username = new char[end - beg + 1];
-	stu.account.username[end - beg] = L'\0';
-	LStrToStr(stu.account.username, end - beg, temp);
-	delete[] temp;
-
-	beg = end + 1;
-	end = str.find(L',', beg);
-	stu.firstname = new wchar_t[end - beg + 1];
-	stu.firstname[end - beg] = L'\0';
-	str.copy(stu.firstname, end - beg, beg);
-
-	beg = end + 1;
-	end = str.find(L',', beg);
-	stu.lastname = new wchar_t[end - beg + 1];
-	stu.lastname[end - beg] = L'\0';
-	str.copy(stu.lastname, end - beg, beg);
-
-	beg = end + 1;
-	end = str.find(L',', beg);
-	temp = new wchar_t[end - beg + 1];
-	temp[end - beg] = L'\0';
-	str.copy(temp, end - beg, beg);
-	if (wcscmp(temp, L"Nam") == 0)stu.gender = 'M';
-	else stu.gender = 'W';
-	delete[] temp;
-
-	beg = end + 1;
-	end = str.find(L',', beg);
-	temp = new wchar_t[end - beg + 1];
-	temp[end - beg] = L'\0';
-	str.copy(temp, end - beg, beg);
-	stu.birth = StringToDate(temp);
-	delete[] temp;
-
-
-	beg = end + 1;
-	end = str.length();
-	temp = new wchar_t[end - beg + 1];
-	temp[end - beg] = L'\0';
-	str.copy(temp, end - beg, beg);
-	stu.SocialID = StringToInt(temp);
-	delete[] temp;
-
-	stu.account.password = new char[7]{ "123456" };
-	return stu;
+bool operator<(tm& t1, tm& t2)
+{
+	time_t a, b;
+	a = mktime(&t1);
+	b = mktime(&t2);
+	if (a < b) return 1;
+	else return 0;
+}
+void SaveLoginHistory(char* AccountUsername)
+{
+	__time32_t now = time(0);
+	char a[100];
+	_ctime32_s(a, &now);
+	std::ofstream file;
+	file.open("history.txt", std::ios::app);
+	file << AccountUsername << " " << a << std::endl;
+	file.close();
 }
 
 
-void AddStudent(_Student*& studentlist, Student student) {
-	if (studentlist == nullptr) { studentlist = new _Student{ student }; studentlist->pNext = studentlist, studentlist->pPrev = studentlist; return; }
-	studentlist->pPrev = new _Student{ student,studentlist,studentlist->pPrev };
-	studentlist->pPrev->pPrev->pNext = studentlist->pPrev;
-}
-
-_Student* FileInStudent(std::string filename) {
-	_LText();
-	std::wfstream fi(filename, std::wfstream::in);
-	if (!fi) { return nullptr; }
-	fi.imbue(std::locale(fi.getloc(), new std::codecvt_utf8<wchar_t>));
-	_Student* stu = nullptr;
-	std::wstring temp;
-	while (fi)
-	{
-		std::getline(fi, temp);
-		if (temp.length() != 0) AddStudent(stu, StringToStudent(temp));
-	}
-	_SText();
-	return stu;
-}
-
-void FileOutStudent(_Student* stu, std::string fileout) {
-	std::wfstream fo(fileout, std::wfstream::out);
-	_LText();
-	fo << L"﻿";
-	fo.imbue(std::locale(fo.getloc(), new std::codecvt_utf8<wchar_t>));
-	_Student* temp = stu;
-	do
-	{
-		fo << stu->student.ID << L',' << stu->student.firstname << L',' << stu->student.lastname << L',' << stu->student.gender << L',' << stu->student.birth << L',' << stu->student.SocialID << L'\n';
-		stu = stu->pNext;
-	} while (stu != temp);
-	fo.close();
-}
-
-
-int NumberOfStudent(_Student* stu) {
-	if (stu == nullptr) return 0;
-	int k = 0;
-	_Student* temp = stu;
-	do {
-		k += 1;
-		stu = stu->pNext;
-	} while (stu != temp);
-	return k;
-}
-void PrintStu(Student* a) {
-	if (a == nullptr) return;
-	_LText();
-	std::wcout << a->ID << "  ";
-	std::wcout << a->firstname << "\t";
-	std::wcout << a->lastname << "\t";
-	std::wcout << a->gender << '\t';
-	std::wcout << a->birth << '\t';
-	std::wcout << a->SocialID << '\t';
-	std::wcout << a->account.username << '\t';
-	std::wcout << a->account.password << '\n';
-	_SText();
-}
-void PrintStu(_Student* stu) {
-	if (stu == nullptr) return;
-	_Student* temp = stu;
-	do
-	{
-		PrintStu(&stu->student);
-		stu = stu->pNext;
-	} while (stu != temp);
-}
-Student* FindStudent(_Student* studentlist, unsigned __int32 ID) {
-	if (studentlist == nullptr) return nullptr;
-	_Student* temp = studentlist;
-	while (studentlist->student.ID != ID && studentlist->pNext != temp)
-	{
-		studentlist = studentlist->pNext;
-	}
-	if (studentlist->student.ID != ID) { return nullptr; }
-	return &studentlist->student;
-}
-void DealloStu(_Student*& stu) {
-	if (stu == nullptr) return;
-	_Student* temp = stu;
-	do {
-		_Student* a = stu;
-		stu = stu->pNext;
-		delete a;
-	} while (stu != temp);
-}
-void RemoveStudent(_Student*& studentlist, unsigned __int32 ID) {
-	if (studentlist == nullptr) return;
-	_Student* temp = studentlist;
-	_Student* pcur = studentlist;
-	while (pcur->student.ID != ID && pcur->pNext != temp)
-	{
-		pcur = pcur->pNext;
-	}
-	if (studentlist->student.ID != ID) return;
-	if (pcur->pNext = pcur) studentlist = nullptr;
-	pcur->pPrev->pNext = pcur->pNext;
-	pcur->pNext->pPrev = pcur->pPrev;
-
-	delete pcur;
-}
-_Student* ConnectStudent(_Student*& a, _Student*& b) {
-	if (a == nullptr) {
-		_Student* temp = b; b = nullptr; return temp;
-	}
-	if (b == nullptr) {
-		_Student* temp = a; a = nullptr; return temp;
-	}
-	a->pPrev->pNext = b;
-	b->pPrev->pNext = a;
-	_Student* temp = a->pPrev;
-	a->pPrev = b->pPrev;
-	b->pPrev = temp;
-	a = nullptr;
-	b = nullptr;
-	return temp->pNext;
-}
