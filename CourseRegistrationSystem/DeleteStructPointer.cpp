59081bfcafdf459efb827c3305a7e72867c5c6d8
Nguyen Phan <goporo090@gmail.com>
2021-04-25 23:59:11 +0700

    delete

---
+++ CourseRegistrationSystem/DeleteStructPointer.cpp
@@ -0,0 +1,12 @@
+void deleteDate() {}
+void deleteAccount(){}
+void deleteScore(){}
+void deleteCourse(){}
+void deleteStudent(){}
+void deleteClasses(){}
+void delete_Student(){}
+void delete_Class(){}
+void delete_Course(){}
+void deleteSemester(){}
+void deleteSchoolYear(){}
+void deleteFilelist() {}
