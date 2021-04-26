d8b003cc451ebddbcc7cba58718401a63dd7d318
Nguyen Phan <goporo090@gmail.com>
2021-04-26 16:08:20 +0700

    delete

--- CourseRegistrationSystem/DeleteStructPointer.cpp
+++ CourseRegistrationSystem/DeleteStructPointer.cpp
@@ -1,12 +1,74 @@
-void deleteDate() {}
-void deleteAccount(){}
-void deleteScore(){}
-void deleteCourse(){}
-void deleteStudent(){}
-void deleteClasses(){}
-void delete_Student(){}
-void delete_Class(){}
-void delete_Course(){}
-void deleteSemester(){}
-void deleteSchoolYear(){}
-void deleteFilelist() {}
+#include"menu.h"
+#include"Data.h"
+#include"CommonFunc.h"
+
+void deleteAccount(Account& a) {
+	delete a.username;
+	delete a.password;
+}
+void deleteScore(Score*& s) {
+	delete s->name;
+	while (s != nullptr) {
+		Score* pTemp = s;
+		s = s->pNext;
+		delete pTemp;
+	}
+
+}
+void deleteCourse(Course& c) {
+	delete c.ID, c.name, c.teacher;
+	delete[] c.performed;
+	deleteScore(c.score);
+}
+void deleteCourseNow(char** c) {
+	int n = _msize(c) / sizeof(char*);
+	for (int i = 0; i < n; i++) {
+		c[i] = nullptr;
+	}
+	delete c, c = nullptr;
+}
+void deleteStudent(Student& s) {
+	delete s.firstname, s.lastname;
+	deleteCourseNow(s.allcourse);
+	deleteCourseNow(s.coursenow);
+	deleteAccount(s.account);
+}
+void deleteClasses(Classes& c) {
+	delete c.name, c.ID;
+}
+void delete_Student(_Student*& s) {
+	while (s != nullptr) {
+		_Student* pTemp = s;
+		s = s->pNext;
+		delete pTemp;
+	}
+}
+void delete_Class(_Class*& s) {
+	while (s != nullptr) {
+		_Class* pTemp = s;
+		s = s->pNext;
+		delete pTemp;
+	}
+}
+void delete_Course(_Course*& s) {
+	while (s != nullptr) {
+		_Course* pTemp = s;
+		s = s->pNext;
+		delete pTemp;
+	}
+}
+void deleteSemester(Semester*& s) {
+	delete_Course(s->course);
+}
+void deleteSchoolYear(SchoolYear*& s) {
+	deleteSemester(s->semester);
+	delete_Class(s->classes);
+	delete_Student(s->student);
+}
+void deleteFilelist(Filelist*& s) {
+	while (s != nullptr) {
+		Filelist* pTemp = s;
+		s = s->pNext;
+		delete pTemp;
+	}
+}
