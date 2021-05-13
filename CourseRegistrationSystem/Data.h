c7464b4c5f46e0bd43b62a2535a0214681075aad
nhatruong1009 <nhattruong1009@gmail.com>
2021-05-13 13:01:08 +0700

    Revert "Revert "Add exit button in schoolplan/login screen, add course test bang input""
    This reverts commit 1e089ed8d3ca0e9e29c91fa1bf9ee488772f2ea0.

--- CourseRegistrationSystem/Data.h
+++ CourseRegistrationSystem/Data.h
@@ -7,6 +7,7 @@
 #include<iomanip>
 #include<codecvt>
 #include<Windows.h>
+#include <conio.h>
 #ifndef _StructData
 #define _StructData
 // the struct use for whole program
@@ -155,7 +156,7 @@ Student* SearchStu(unsigned __int64 ID);
 Student** SearchStuArr(unsigned __int64* ID, std::string Grade);
 std::string GetFilePath(unsigned __int64 ID);
 
-void SaveNewStu(_Student* stu,char* directon);
+void SaveNewStu(_Student* stu, char* directon);
 void FileOutStudent(_Student* stu, std::string fileout);
 void StuToBin(Student* stu, std::string fileout);
 void AddStudent(_Student*& studentlist, Student* student);
